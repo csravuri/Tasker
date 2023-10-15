8e93c94284fe2c3920763ff4e3ec2aee2dd6648d
Chandra Sekhar Ravuri <35503656+csravuri@users.noreply.github.com>
2023-10-15 23:21:08 +0530

    move xamarin forms to maui

---
+++ Tasker/Models/BaseModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tasker.Models
+{
+    public class BaseModel
+    {
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+        public bool Save()
+        {
+            return false;
+        }
+
+        public bool Delete()
+        {
+            return false;
+        }
+    }
+}
---
+++ Tasker/Views/TaskView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Tasker.ViewModels;
+using Microsoft.Maui.Controls.Xaml;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui;
+
+namespace Tasker.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TaskView : ContentPage
+    {
+        private TaskViewViewModel viewModel;
+
+        public TaskView()
+        {
+            InitializeComponent();
+            BindingContext = viewModel = new TaskViewViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            viewModel.OnAppearing();
+        }
+    }
+}
